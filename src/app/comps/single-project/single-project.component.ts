06d338eab1317052f91079d10d8ca67f39d408d0
Andrew <andrewjacobalvarez22@gmail.com>
2023-06-13 15:24:03 -0500

    input field or left or right side will be auto

---
+++ src/app/comps/single-project/single-project.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-single-project',
+  templateUrl: './single-project.component.html',
+  styleUrls: ['./single-project.component.css'],
+})
+export class SingleProjectComponent {
+  @Input() position: string = '';
+}
