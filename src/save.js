b1d8705dd888445623c3f0f17697e5c917f34da2
Andrew <andrewjacobalvarez22@gmail.com>
2023-06-13 14:21:16 -0500

    saved data that I acidentally lost

---
+++ src/save.js
@@ -0,0 +1,74 @@
+// #rainbowEff {
+//     background: linear-gradient(
+//       to right,
+//       red,
+//       orange,
+//       yellow,
+//       green,
+//       blue,
+//       indigo,
+//       violet,
+//       grey
+//     );
+//     background-clip: text;
+//     color: transparent;
+//     animation: rainbowAni 6s linear infinite;
+//     background-size: 400%;
+//   }
+//   @keyframes rainbowAni {
+//     0% {
+//       background-position: 0;
+//     }
+//     50% {
+//       background-position: 100%;
+//     }
+//     100% {
+//       background-position: 0;
+//     }
+//   }
+// <div
+//   class="text-9xl mt-28 w-full justify-center h-full inline-flex space-x-12 mb-36"
+// >
+//   <div class="rotate-45 rounded-3xl w-fit p-1 bg-clip-text" id="rainbowEff">
+//     P
+//   </div>
+//   <div
+//     class="translate-y-16 -rotate-12 rounded-3xl w-fit p-1 bg-clip-text"
+//     id="rainbowEff"
+//   >
+//     R
+//   </div>
+//   <div
+//     class="translate-y-12 -rotate-45 rounded-3xl w-fit p-1 bg-clip-text"
+//     id="rainbowEff"
+//   >
+//     O
+//   </div>
+//   <div
+//     class="translate-y-20 -rotate-12 rounded-3xl w-fit p-1 bg-clip-text"
+//     id="rainbowEff"
+//   >
+//     J
+//   </div>
+//   <div
+//     class="translate-y-24 rotate-45 rounded-3xl w-fit p-1 bg-clip-text"
+//     id="rainbowEff"
+//   >
+//     E
+//   </div>
+//   <div
+//     class="translate-y-14 rotate-180 rounded-3xl w-fit p-1 bg-clip-text"
+//     id="rainbowEff"
+//   >
+//     C
+//   </div>
+//   <div
+//     class="translate-y-12 -rotate-45 rounded-3xl w-fit p-1 bg-clip-text"
+//     id="rainbowEff"
+//   >
+//     T
+//   </div>
+//   <div class="-rotate-12 rounded-3xl w-fit p-1 bg-clip-text" id="rainbowEff">
+//     S
+//   </div>
+// </div>
