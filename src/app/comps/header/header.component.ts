19cffe2a6f97cf7c013a0aa642ef7720650e050f
Andrew <andrewjacobalvarez22@gmail.com>
2023-06-21 15:32:27 -0500

    menuOpen + togglemenu command

--- src/app/comps/header/header.component.ts
+++ src/app/comps/header/header.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Directive, HostListener } from '@angular/core';
+// import { slide as Menu } from 'react-burger-menu';
 import {
   faComputerMouse,
   faMailReplyAll,
 } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  // Declare a boolean variable to store the menu state
+  menuOpen: boolean = false;
+
+  // Define a function to toggle the menu state
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
   faComputerMouse = faComputerMouse;
   faMailReplyAll = faMailReplyAll;
 }
