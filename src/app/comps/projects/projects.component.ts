84b14266d14739eb03b00d329fd5c66b2f67ae63
Andrew <andrewjacobalvarez22@gmail.com>
2023-06-30 12:18:09 -0500

    text changes when sizing < 125

--- src/app/comps/projects/projects.component.ts
+++ src/app/comps/projects/projects.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
+  template: `
+    <div #container class="container">
+      <div #flexboxes id="flexboxes">Content</div>
+    </div>
+  `,
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+  styleUrls: ['./projects.component.css'],
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements AfterViewInit {
+  private resizeObserver!: ResizeObserver;
 
+  constructor(private elementRef: ElementRef) {}
+
+  ngAfterViewInit() {
+    const flexboxes =
+      this.elementRef.nativeElement.querySelectorAll('.flexboxes');
+
+    this.resizeObserver = new ResizeObserver((entries) => {
+      for (let entry of entries) {
+        const target = entry.target as HTMLElement;
+        if (target.clientWidth < 125) {
+          target.style.alignItems = 'center';
+          target.style.paddingLeft = '0px';
+          target.style.transition = '.8s';
+        } else {
+          target.style.alignItems = '';
+          target.style.paddingLeft = '';
+        }
+      }
+    });
+
+    for (let entry of flexboxes) {
+      this.resizeObserver.observe(entry);
+    }
+  }
+
+  ngOnDestroy() {
+    this.resizeObserver.disconnect();
+  }
 }
