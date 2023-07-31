123199b4386565274cdb4e057cb4521f8d788c13
Andrew <andrewjacobalvarez22@gmail.com>
2023-07-30 21:27:29 -0500

    nav button no nav bar

---
+++ src/app/comps/navbutton/navbutton.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbutton',
+  templateUrl: './navbutton.component.html',
+  styleUrls: ['./navbutton.component.css']
+})
+export class NavbuttonComponent {
+
+}
