cc24966fa303219cd1480cfbfd25119afb3c51c6
Andrew <andrewjacobalvarez22@gmail.com>
2023-08-01 15:12:24 -0500

    image changer

--- src/app/comps/carousel/carousel.component.ts
+++ src/app/comps/carousel/carousel.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.css']
+  styleUrls: ['./carousel.component.css'],
+  animations: [
+    trigger('wordChange', [
+      state('react', style({})),
+      state('nextjs', style({})),
+      state('svelte', style({})),
+      state('angularjs', style({})),
+      transition('* => *', [animate('3s')]),
+    ]),
+  ],
 })
 export class CarouselComponent {
+  currentWord = 'react';
+  words = ['react', 'nextjs', 'svelte', 'angularjs'];
+  index = 0;
 
+  constructor() {
+    setInterval(() => {
+      this.index = (this.index + 1) % this.words.length;
+      this.currentWord = this.words[this.index];
+    }, 3000);
+  }
 }
