40175bcf7be2f6b0484bc785fdb414b0c5be0dd4
Andrew <andrewjacobalvarez22@gmail.com>
2023-08-28 14:07:01 -0500

    removed logs

--- src/app/comps/carousel2/carousel2.component.ts
+++ src/app/comps/carousel2/carousel2.component.ts
@@ -17,27 +17,20 @@ export class Carousel2Component implements AfterViewInit {
     const slide = this.elementRef.nativeElement.querySelectorAll('#boxes');
     const background =
       this.elementRef.nativeElement.querySelectorAll('#purpleBG');
-    // console.log('dwadwaawdawdwawadadwad', slide);
 
     this.resizeObserver = new ResizeObserver((projects) => {
       for (let project of projects) {
-        // console.log(project, 'background');
-
         const target = project.target as HTMLElement;
-        // const backg = project.target as HTMLElement;
-        // console.log(background);
+        console.log(target.clientHeight);
         if (target.clientHeight > 395) {
           this.elementView.nativeElement.style.height = '1450px';
         } else {
           this.elementView.nativeElement.style.height = '1240px';
         }
-        console.log(background);
-        // console.log(target.clientWidth);
       }
     });
 
     for (let project of slide) {
-      // console.log(project, 'testestest');
       this.resizeObserver.observe(project);
     }
   }
